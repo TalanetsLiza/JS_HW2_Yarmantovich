f642c2f2275326ba6c53e763d126b6af28289097
TalanetsLiza <yarmantovich1999@mail.ru>
2023-03-05 15:06:15 +0300

    add: complete task 3

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+"use strict";
+
+{
+    // Task 3
+
+    const num = "711117";
+    if ((+num[0] + +num[1] + +num[2]) === (+num[3] + +num[4] + +num[5])) {
+        console.log ("Да");
+    } else {
+        console.log ("Нет");
+    }
+    
+}
